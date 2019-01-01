afee3f05b173847b45f0a2c3167301b9cbf3f937
yokoo <takyokoo@gmail.com>
2019-01-01 16:03:29 +0900

    fix build script.

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
+cd `dirname $0`
+rm -rf bin
+mkdir bin
+
 for ARCH in 386 amd64 ; do
 	OS=windows
-	mkdir -p "bin/$OS/$ARCH"
 	GOOS="$OS" GOARCH="$ARCH" go build -ldflags "-s -w"
-	mv oogrep.exe "bin/$OS/$ARCH"
-	zip -r "bin/${OS}_${ARCH}.zip" "bin/$OS/$ARCH"
+	zip -r "bin/${OS}_${ARCH}.zip" oogrep.exe
+	rm oogrep.exe
 
 	for OS in darwin linux; do
-		mkdir -p "bin/$OS/$ARCH"
 		GOOS="$OS" GOARCH="$ARCH" go build -ldflags "-s -w"
-		mv oogrep "bin/$OS/$ARCH"
-		tar -zcvf "bin/${OS}_${ARCH}.tgz" "bin/$OS/$ARCH"
+		tar -zcvf "bin/${OS}_${ARCH}.tgz" oogrep
 	done
 done
 
